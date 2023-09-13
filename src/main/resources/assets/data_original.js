692aecea80f49b62e172e1d193500b9c65fbda4f
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2023-09-13 13:19:24 +0100

    PLATUI-2499 - Refactor (VR|KB|DE)

--- src/main/resources/assets/data_original.js
+++
@@ -1,6 +0,0 @@
-function reportData() {
-    const reportMetaData = '%INJECT_REPORT_METADATA%';
-    const axeAssessedPages = ['%INJECT_AXE_VIOLATIONS%'];
-
-    return {reportMetaData, axeAssessedPages}
-}

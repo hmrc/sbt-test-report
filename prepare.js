fc41ecdebc840c3d8250f44720025f6b825973b2
Kyle Bowden <1393788+kyle-bowden@users.noreply.github.com>
2023-09-15 17:48:58 +0100

    Development scaffolding setup

---
+++ prepare.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require("path");
+
+// Define the data you want to inject
+const reportMetaData = require("./src/test/resources/report_meta_data.json");
+const axeAssessedPages = require("./src/test/resources/axe_results.json");
+
+// const reportMetaData = {name: 'Your report metadata here'};
+// const axeAssessedPages = [{a:'Page 1', b:'Page 2', c:'Page 3'}]; // Example values
+
+const dataJsFile = __dirname + '/src/main/resources/assets/data.js';
+
+// Define a path to the original data.js file and a backup file
+const originalDataJsPath = path.join(__dirname, '/src/main/resources/assets/data.js');
+const backupDataJsPath = path.join(__dirname, '/src/main/resources/assets/data.js.bak'); // Backup file name
+
+const startServer = async () => {
+    // Check if a backup of data.js exists, and if not, create one from the original
+    if (!fs.existsSync(backupDataJsPath)) {
+        fs.copyFileSync(originalDataJsPath, backupDataJsPath);
+    }
+
+    // Read the template data.js file
+    await fs.readFile(dataJsFile, 'utf8', async (err, data) => {
+        if (err) {
+            console.error('Error reading data.js:', err);
+            return;
+        }
+
+        // Replace the placeholders with the actual data
+        data = data
+            .replace('INJECT_REPORT_METADATA', JSON.stringify(reportMetaData))
+            .replace('INJECT_AXE_VIOLATIONS', JSON.stringify(axeAssessedPages));
+
+        // Write the updated content back to data.js
+        await fs.writeFile(dataJsFile, data, 'utf8', (err) => {
+            if (err) {
+                console.error('Error writing data.js:', err);
+                return;
+            }
+            console.log('data.js updated successfully.');
+        });
+    });
+}
+
+startServer();
+
+

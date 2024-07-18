b98aca1895d3b95883c742754d24708867488ec4
wayne ellis <9093361+funkmaster20th@users.noreply.github.com>
2024-07-18 10:38:49 +0100

    PLATUI-3150 added 2 unit tests in exclusionfilter to test for backlink in a wrapped snippet

--- src/test/scala/uk/gov/hmrc/testreport/model/ExclusionFilterSpec.scala
+++ src/test/scala/uk/gov/hmrc/testreport/model/ExclusionFilterSpec.scala
@@ -18,6 +18,7 @@ package uk.gov.hmrc.testreport.model
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+import uk.gov.hmrc.testreport.model.PlatformExclusionRules.AuthLoginStub.{maybeHtmlRegex, maybePathRegex, reason}
 
 class ExclusionFilterSpec extends AnyWordSpec with Matchers {
 
@@ -105,6 +106,34 @@ class ExclusionFilterSpec extends AnyWordSpec with Matchers {
         impact = "moderate",
         html = """<a href="#" class="govuk-back-link  js-visible">Back</a>""",
         exclusionRules = Nil
+      ),
+      AxeViolation(
+        url =
+          "http://localhost:9706/self-employment-support/account/sign-in-required?continueUrl=http%3A%2F%2Flocalhost%3A9706%2Fself-employment-support%2Fclaim%2Fyour-claims%3Fk%3Dsession-e1794f19-ed40-47e6-b30e-24936aa12ab5",
+        help = "All page content should be contained by landmarks",
+        helpUrl = "https://dequeuniversity.com/rules/axe/4.8/region?application=axeAPI",
+        impact = "moderate",
+        html = """<div class="govuk-grid-column-one-half back-link">
+                 |
+                 |                <a href="#" class="govuk-back-link" id="back-link">Back</a>
+                 |
+                 |
+                 |
+                 |        </div>""".stripMargin,
+        exclusionRules = Nil
+      ),
+      AxeViolation(
+        url =
+          "http://localhost:10108/help-customer-tax-check/personal-details",
+        help = "All page content should be contained by landmarks",
+        helpUrl = "https://dequeuniversity.com/rules/axe/4.8/region?application=axeAPI",
+        impact = "moderate",
+        html = """<div class="govuk-!-display-none-print">
+                 |    <a href="/help-customer-tax-check/who-tax-check-for" class="govuk-back-link" id="back">Back</a>
+                 |
+                 |
+                 |  </div>""".stripMargin,
+        exclusionRules = Nil
       )
     )
   }
@@ -120,7 +149,7 @@ class ExclusionFilterSpec extends AnyWordSpec with Matchers {
 
       val (exclViolations, inclViolations) = partitionViolations(rawAxeViolations, exclusionRules)
       exclViolations.length shouldBe 0
-      inclViolations.length shouldBe 10
+      inclViolations.length shouldBe 12
     }
 
     "exclude violations if rules match" in new Setup {
@@ -137,7 +166,7 @@ class ExclusionFilterSpec extends AnyWordSpec with Matchers {
 
       val (exclViolations, inclViolations) = partitionViolations(rawAxeViolations, exclusionRules)
       exclViolations.length shouldBe 5
-      inclViolations.length shouldBe 5
+      inclViolations.length shouldBe 7
 
       exclViolations.map(_.exclusionRules) shouldBe List(
         List(
@@ -167,7 +196,7 @@ class ExclusionFilterSpec extends AnyWordSpec with Matchers {
 
       val (exclViolations, inclViolations) = partitionViolations(rawAxeViolations, exclusionRules)
       exclViolations.length shouldBe 2
-      inclViolations.length shouldBe 8
+      inclViolations.length shouldBe 10
     }
 
     "require exclusion paths containing special characters to be escaped" in new Setup {
@@ -180,13 +209,26 @@ class ExclusionFilterSpec extends AnyWordSpec with Matchers {
 
       val (exclViolations, inclViolations) = partitionViolations(rawAxeViolations, exclusionRules)
       exclViolations.length shouldBe 1
-      inclViolations.length shouldBe 9
+      inclViolations.length shouldBe 11
     }
 
     "exclude violations defined in Platform Exclusion Rules" in new Setup {
       val (exclViolations, inclViolations) = partitionViolations(rawAxeViolations, PlatformExclusionRules.all)
-      exclViolations.length shouldBe 10
+      exclViolations.length shouldBe 12
       inclViolations        shouldBe empty
+    }
+    "exclude violations defined if just backlink" in new Setup {
+      val exclusionRules:  List[PlatformExclusionRule] = List(
+        PlatformExclusionRule(
+          maybeHtmlRegex = Some(RegexPattern(""".*class="govuk-back-link.*""")),
+          None,
+          reason = "Some other reason"
+        )
+
+      )
+      val (exclViolations, inclViolations) = partitionViolations(rawAxeViolations, exclusionRules)
+      exclViolations.length shouldBe 12
+      inclViolations shouldBe empty
     }
   }
 }
